731c91738fa7688679a83231ea092635018af0c8
Dan Davies Brackett <dan@daviesbrackett.com>
2024-03-26 18:58:56 +0000

    show expenses

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expense;
+use App\Models\SchoolClass;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class ExpenseController extends Controller
 {
-    //
+    function expenses(): View
+    {
+        return $this->result();
+    }
+
+    function postExpenses(): View
+    {
+        return view('admin.expenses.show)');
+    }
+
+    private function result($extra = [])
+    {
+        $expenses = Expense::orderby('expense_date', 'desc')->with('schoolclass')->get();
+        $schoolclasses = [];
+        foreach (SchoolClass::all() as $sc) {
+            $schoolclasses[$sc->id] = $sc->name;
+        }
+        return view('admin.expenses.show', array_merge(['model' => $expenses, 'schoolclasses' => $schoolclasses], $extra));
+    }
 }
