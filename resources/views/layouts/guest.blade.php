a501e5e6034b68ce880a5578733e0c59ab687d2a
Dan Davies Brackett <dan@daviesbrackett.com>
2024-10-06 18:27:11 +0100

    show order edit in nav

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -36,9 +36,15 @@
                             <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                                 Home
                             </x-nav-link>
-                            <x-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
-                                Order
-                            </x-nav-link>
+                            @auth
+                                <x-nav-link href="{{ route('edit') }}" :active="request()->routeIs('edit')">
+                                    Order
+                                </x-nav-link>
+                            @else
+                                <x-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+                                    Order
+                                </x-nav-link>
+                            @endauth
                             <x-nav-link href="{{ route('leaderboard') }}" :active="request()->routeIs('leaderboard')">
                                 Leaderboard
                             </x-nav-link>
@@ -75,9 +81,15 @@
                     <x-responsive-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                         Home
                     </x-responsive-nav-link>
-                    <x-responsive-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
-                        Order
-                    </x-responsive-nav-link>
+                    @auth
+                        <x-responsive-nav-link href="{{ route('edit') }}" :active="request()->routeIs('edit')">
+                            Order
+                        </x-responsive-nav-link>
+                    @else
+                        <x-responsive-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+                            Order
+                        </x-responsive-nav-link>
+                    @endauth
                     <x-responsive-nav-link href="{{ route('leaderboard') }}" :active="request()->routeIs('leaderboard')">
                         Leaderboard
                     </x-responsive-nav-link>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                         Home
                     </x-nav-link>
+                    <x-nav-link href="{{ route('edit') }}" :active="request()->routeIs('edit')">
+                        Order
+                    </x-nav-link>
                     <x-nav-link href="{{ route('account') }}" :active="request()->routeIs('account')">
                         Account
                     </x-nav-link>
@@ -114,6 +117,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                 Home
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('edit') }}" :active="request()->routeIs('edit')">
+                Order
             </x-responsive-nav-link>
             <x-responsive-nav-link href="{{ route('account') }}" :active="request()->routeIs('account')">
                 Account
