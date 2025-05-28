492e00dfbc4c1ae6d32b0893b267245ef05d687d
Dan Davies Brackett <dan@daviesbrackett.com>
2025-05-28 17:49:46 +0100

    clarify task scheduling
    along with this comes the move to using artisan schedule:run rather than specific command invocations

--- routes/console.php
+++ routes/console.php
@@ -7,14 +7,12 @@ use App\Console\Commands\GenerateOrders;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(DoDeadlineReminder::class,
-                 [Carbon::now()->format('Y-m-d')])->timezone('America/Los_Angeles')->dailyAt('01:30');
+Schedule::command(DoDeadlineReminder::class, ['now'])->timezone('America/Los_Angeles')->dailyAt('01:30');
 
-Schedule::command(GenerateOrders::class,
-                 [Carbon::now()->format('Y-m-d')])->timezone('America/Los_Angeles')->dailyAt('02:30');
+Schedule::command(GenerateOrders::class, ['now'])->timezone('America/Los_Angeles')->dailyAt('02:30');
 
-Schedule::command(ChargeCreditCards::class,
-                 [Carbon::now()->format('Y-m-d')])->timezone('America/Los_Angeles')->dailyAt('05:00');
+Schedule::command(ChargeCreditCards::class, ['now'])->timezone('America/Los_Angeles')->dailyAt('05:00');
 
-Schedule::command(DoPickupReminder::class,
-                 [Carbon::now()->format('Y-m-d')])->timezone('America/Los_Angeles')->dailyAt('07:00');
+Schedule::command(DoPickupReminder::class, ['now'])->timezone('America/Los_Angeles')->dailyAt('07:00');
+
+Schedule::command("queue:work", ["--stop-when-empty"])->everyMinute()->withoutOverlapping();
