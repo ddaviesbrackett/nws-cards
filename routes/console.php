9a3376bdbca7d4018fbb26a8c45032de699b23aa
Dan Davies Brackett <dan@daviesbrackett.com>
2025-08-09 13:49:15 +0100

    turn off pickup reminders

--- routes/console.php
+++ routes/console.php
@@ -12,7 +12,8 @@ Schedule::command(DoDeadlineReminder::class, ['now'])->timezone('America/Los_Ang
 Schedule::command(GenerateOrders::class, ['now'])->timezone('America/Los_Angeles')->dailyAt('02:30');
 
 Schedule::command(ChargeCreditCards::class, ['now'])->timezone('America/Los_Angeles')->dailyAt('05:00');
-
+/*
+pickup reminders are disabled per PAC - functionality to be replaced by calendar reminders
 Schedule::command(DoPickupReminder::class, ['now'])->timezone('America/Los_Angeles')->dailyAt('07:00');
-
+*/
 Schedule::command("queue:work", ["--stop-when-empty"])->everyMinute()->withoutOverlapping();
